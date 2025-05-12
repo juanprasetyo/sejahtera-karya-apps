dfa12c05c4d1f8582cea88950446995acc9078a4
Juan Prasetyo <juanprasetyo000@gmail.com>
2025-05-12 11:45:32 +0700

    build(vite): auto-load JS and CSS entries using fast-glob

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import fg from 'fast-glob';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: fg.sync([
+                'resources/js/**/*.js',
+                'resources/css/**/*.css',
+            ]),
             refresh: true,
         }),
         tailwindcss(),
